003f69e906c2944c5702fe49c43f1f7192daafad
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2021-11-22 00:51:11 -0600

    Update HealthPack.cpp

--- Space Blasters/HealthPack.cpp
+++ Space Blasters/HealthPack.cpp
@@ -27,6 +27,16 @@ const sf::FloatRect HealthPack::GetGlobalBounds() const
 	return healthPackSprite.getGlobalBounds();
 }
 
+const sf::Vector2f& HealthPack::GetPosition() const
+{
+	return healthPackSprite.getPosition();
+}
+
+void HealthPack::SetPosition(float pos_X, float pos_Y)
+{
+	healthPackSprite.setPosition(pos_X, pos_Y);
+}
+
 // Public functions
 void HealthPack::Update()
 {
